46365b5ccd4211cb7a3abfc9495350d77b8aaa86
John Albright <John@john-albrights-macbook-pro.local>
2021-10-03 04:11:45 -0500

    Modified mobile styles for the home page; replaced a back end project on the home page; added two sections to the all projects page and jQuery interaction with the Expand All button; fixed the inconsistencies and disorganization of the footer styles page and footer color styles at the bottom of other style css documents

--- javascript/other-scripts.js
+++ javascript/other-scripts.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
 
-    // Add click event listener for the divs 
+    // Add click event listener for the grid expander 
     $(".grid-expander").on("click", function() {
         $(this).parents(".grid-unit").find(".hidden-info").slideToggle(300);
         $(this).toggleClass("rotate-expander");
     });
+
+    // Add click event for the div expanders on the projects page
+
+    $(".expand-all-projects").on("click", function() {
+        
+        const expandText = "Expand All";
+        const collapseText = "Collapse All";
+        var text = $(this).text();
+        //console.log(text);
+        
+        // Toggle the text in the button and rotate all div expanders
+        if (text.includes(expandText)) {
+            $(".expand-all-projects").text(collapseText);
+            $(".hidden-info").slideDown();
+            $(".grid-expander").addClass("rotate-expander");
+        }
+        
+        if (text.includes(collapseText)) {
+            $(".expand-all-projects").text(expandText);
+            $(".hidden-info").slideUp();
+            $(".grid-expander").removeClass("rotate-expander");
+        }
+        
+    });
+
 });
